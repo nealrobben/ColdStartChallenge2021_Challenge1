3ef8b295f78e473f6dbc594a99f20fab754354f0
nealrobben <neal.robben@cegeka.com>
2021-02-25 17:48:02 +0100

    Commented out lines to find error

--- api/orders-post/index.js
+++ api/orders-post/index.js
@@ -1,6 +1,6 @@
 const { getUser } = require('../shared/user-utils');
 const { QueueClient } = require("@azure/storage-queue");
-const uuidv1 = require("uuid/v1");
+//const uuidv1 = require("uuid/v1");
 
 module.exports = async function (context, req) {
   // Get the user details from the request
@@ -12,9 +12,9 @@ module.exports = async function (context, req) {
   // TODO: add the pre-order JSON document in a queue
   
   const connectionString = "DefaultEndpointsProtocol=https;AccountName=cscchallenge1;AccountKey=6y4UPgZHkguAyR9rDWqH0VfirDI+oBZz+3AK8clTzmXsUkUmgo23O7x/IHsHfziRGpoH+I+lIahXaaLQY/606w==;EndpointSuffix=core.windows.net"
-  const queueClient = new QueueClient(connectionString, "orders");
+  // const queueClient = new QueueClient(connectionString, "orders");
   
-  await queueClient.sendMessage("Test");
+  // await queueClient.sendMessage("Test");
   
   context.res.status(201);
 };
