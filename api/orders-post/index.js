834d61361fed3811719dbe1224339125c1c30469
nealrobben <neal.robben@cegeka.com>
2021-02-25 17:54:19 +0100

    Reenabled queue client

--- api/orders-post/index.js
+++ api/orders-post/index.js
@@ -12,7 +12,7 @@ module.exports = async function (context, req) {
   // TODO: add the pre-order JSON document in a queue
   
   const connectionString = "DefaultEndpointsProtocol=https;AccountName=cscchallenge1;AccountKey=6y4UPgZHkguAyR9rDWqH0VfirDI+oBZz+3AK8clTzmXsUkUmgo23O7x/IHsHfziRGpoH+I+lIahXaaLQY/606w==;EndpointSuffix=core.windows.net"
-  // const queueClient = new QueueClient(connectionString, "orders");
+  const queueClient = new QueueClient(connectionString, "orders");
   
   // await queueClient.sendMessage("Test");
   
